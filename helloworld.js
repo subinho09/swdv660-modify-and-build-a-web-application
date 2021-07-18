d40cb189b3a29ae78198bb7f2c4137da9e16c5e8
subinho09 <83929008+subinho09@users.noreply.github.com>
2021-07-18 00:15:05 -0500

    tests

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello Week 3\n')
-}).listen(3333)
+}).listen(3000)
 
 // Console will print the message
 console.log('Server running')
