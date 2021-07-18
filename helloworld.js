b70a974e36e1fb815935fb50651e0287f7714cde
subinho09 <83929008+subinho09@users.noreply.github.com>
2021-07-18 00:03:25 -0500

    Change

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Welcome to Week 3 | Assignment : Update CloudFormation template\n')
+   response.end('Hello Week 3\n')
 }).listen(3333)
 
 // Console will print the message
